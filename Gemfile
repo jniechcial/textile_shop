f614845a01bc459d62e0c322cedadca09715d9d7
Jakub Niechcial <jakub.niechcial@intercom.io>
2018-02-28 20:44:14 +0000

    initial run on navbar

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'spree', '~> 3.4.4'
 gem 'spree_auth_devise', '~> 3.3'
 gem 'spree_gateway', '~> 3.3'
 
+gem 'font-awesome-sass', '~> 5.0.6'
+
 gem 'spree_i18n', github: 'spree-contrib/spree_i18n', branch: 'master'
 
 group :development, :test do
---
+++ app/overrides/base_helper_decorator.rb
@@ -0,0 +1,17 @@
+Spree::FrontendHelper.module_eval do
+
+  def link_to_cart(text = nil)
+    css_class = nil
+
+    if simple_current_order.nil? || simple_current_order.item_count.zero?
+      text = "<div class='cart-icon-wrapper'><div class='cart-icon'><i class='fas fa-shopping-bag'></i></div><div class='cart-size'>0</div></div>"
+      css_class = 'empty'
+    else
+      text = "<div class='cart-icon-wrapper'><div class='cart-icon'><i class='fas fa-shopping-bag'></i></div><div class='cart-size'><span>#{simple_current_order.item_count}</div></div>"
+      css_class = 'full'
+    end
+
+    link_to text.html_safe, spree.cart_path, class: "cart-info #{css_class}"
+  end
+
+end
